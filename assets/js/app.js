bac69c6e066d53277025f31dc4724036b01d7cf5
SofiaAR <sofia.arevalor@gmail.com>
2017-12-14 16:51:43 -0300

    reto lyft jquery

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal1').modal();
+
+  });
+        $(function(){
+   setTimeout(function() {
+      $('#splash').fadeOut(500);
+   }, 1000);
+})
